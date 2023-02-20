77fdc3624cec5cb7a6ba1da95594344827d511e2
giangbang <banggiangle2015@gmail.com>
2023-02-20 23:37:56 +0700

    fix video device not available in L6 github check

--- AdvProg_L6-Painter/utils.cpp
+++ AdvProg_L6-Painter/utils.cpp
@@ -8,7 +8,7 @@ SDL_Color dict2SDL_Color(int r, int g, int b) {
 	return color;
 }
 
-bool validColorValue(int value) { 
+bool validColorValue(int value) {
 	if (value >=0 && value <= 255) return true;
 	else return false;
 }
@@ -18,10 +18,20 @@ void logSDLError(std::ostream& os, const std::string &msg)
 	os << msg << " Error: " << SDL_GetError() << std::endl;
 }
 
+void initSDLsoftware(SDL_Window **window, SDL_Renderer **renderer) {
+	// create a virtual software renderer without a window, used when video device is not available
+	SDL_Surface* surface = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32,0,0,0,0);
+	*renderer = SDL_CreateSoftwareRenderer(surface);
+	*window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_HIDDEN);
+}
+
 void initSDL(SDL_Window **window, SDL_Renderer **renderer) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		logSDLError(std::cout, "SDL_Init");
-		exit(1);
+		std::cout << "The program will still continue without a video device!" << std::endl;
+		initSDLsoftware(window, renderer);
+		return;
+		// exit(1);
 	}
 	*window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	//window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
@@ -54,15 +64,14 @@ void quitSDL(SDL_Window **window, SDL_Renderer **renderer)
 void waitUntilKeyPressed()
 {
 	SDL_Event e;
-	bool keyPressed = false;
-	while (!keyPressed) {
-		if (SDL_WaitEvent(&e) != 0) {
-			if (e.type == SDL_KEYDOWN) {
-				keyPressed = true;
-			} else if (e.type == SDL_QUIT) {
-				keyPressed = true;
-			}
-		}
-		SDL_Delay(10000);
+	bool quit = false;
+	while (!quit) {
+		while( SDL_PollEvent( &e ) != 0 ) {
+	    //User requests quit
+	    if( e.type == SDL_QUIT || e.type == SDL_KEYDOWN )
+	    {
+	        quit = true;
+	    }
+	  }
 	}
 }
