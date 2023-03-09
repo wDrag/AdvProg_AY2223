65e5e8eeda66c3583e659e4ebc28f00d5b3b4789
Hoàng Công Vinh <haxorus5224@gmail.com>
2023-03-09 18:46:01 +0700

    modified painter.cpp

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -71,7 +71,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
-    angle -= degree;
+    angle = angle - degree;
     compress(angle);
 }
 
